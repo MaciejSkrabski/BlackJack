575a76f33594089af7a481bb93d5d1f8394ef0b3
MaciejSkrabski <maciej.skrabski@gmail.com>
2019-06-13 03:12:42 +0200

    first fields

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -28,7 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.hit = new System.Windows.Forms.Button();
+            this.stand = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // hit
+            // 
+            this.hit.Location = new System.Drawing.Point(713, 352);
+            this.hit.Name = "hit";
+            this.hit.Size = new System.Drawing.Size(75, 40);
+            this.hit.TabIndex = 0;
+            this.hit.Text = "Hit";
+            this.hit.UseVisualStyleBackColor = true;
+            // 
+            // stand
+            // 
+            this.stand.Location = new System.Drawing.Point(713, 398);
+            this.stand.Name = "stand";
+            this.stand.Size = new System.Drawing.Size(75, 40);
+            this.stand.TabIndex = 1;
+            this.stand.Text = "Stand";
+            this.stand.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Twoje karty:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(12, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Suma:";
             // 
             // Form1
             // 
@@ -36,6 +78,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stand);
+            this.Controls.Add(this.hit);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -47,10 +93,16 @@
             this.Text = "Black Jack";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button hit;
+        private System.Windows.Forms.Button stand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
